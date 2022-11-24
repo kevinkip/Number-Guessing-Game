4e1cc2b7506cb9318243decabed84f2b00698f05
Kevin Kipkemboi <kkipkemboi@live.com>
2022-11-24 00:45:20 -0800

    Add files via upload

---
+++ number_guess.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo -n "Enter your username:"
+read USERNAME 
+# get player_id from games table
+USERNAME_CHECK=$($PSQL "SELECT DISTINCT name FROM players WHERE name LIKE '$USERNAME'")
+# extract number of games played
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games JOIN players USING(player_id) WHERE name='$USERNAME'")
+# extract number of guesses from best game
+BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games JOIN players USING(player_id) WHERE name='$USERNAME'")
+if [[ "$USERNAME_CHECK" != "$USERNAME" ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # insert new player
+  INSERT_NEW_PLAYER=$($PSQL "INSERT INTO players(name) VALUES('$USERNAME')")
+elif [[ "$USERNAME_CHECK" == "$USERNAME" ]]
+then
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+# Game logic starts here
+SECRET_NUMBER=$(( ($RANDOM % 1000) + 1 ))
+echo -n "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=1
+while read NUMBER_GUESS
+do
+  if [[ ! $NUMBER_GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else 
+    if [[ $NUMBER_GUESS == $SECRET_NUMBER ]]
+    then
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+      PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name='$USERNAME'")
+      INSERT_PLAYER_ID=$($PSQL "INSERT INTO games(player_id, number_of_guesses) VALUES($PLAYER_ID, $NUMBER_OF_GUESSES)")
+    break;
+    else 
+      if [[ $NUMBER_GUESS -gt $SECRET_NUMBER ]]
+      then
+        echo -n "It's lower than that, guess again:"
+        let NUMBER_OF_GUESSES++
+      elif [[ $NUMBER_GUESS -lt $SECRET_NUMBER ]]
+      then
+        echo -n "It's higher than that, guess again:"
+        let NUMBER_OF_GUESSES++
+      fi
+    fi
+  fi
+done
+
+
+
+
+
+ 
+
+
---
+++ number_guess.sql
@@ -0,0 +1,160 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: number_guess; Type: DATABASE PROPERTIES; Schema: -; Owner: freecodecamp
+--
+
+ALTER ROLE freecodecamp IN DATABASE number_guess SET tcp_keepalives_idle TO '30000';
+
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: games; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.games (
+    player_id integer,
+    number_of_guesses integer
+);
+
+
+ALTER TABLE public.games OWNER TO freecodecamp;
+
+--
+-- Name: players; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.players (
+    player_id integer NOT NULL,
+    name character varying(25) NOT NULL
+);
+
+
+ALTER TABLE public.players OWNER TO freecodecamp;
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.players_player_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.players_player_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.players_player_id_seq OWNED BY public.players.player_id;
+
+
+--
+-- Name: players player_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.players ALTER COLUMN player_id SET DEFAULT nextval('public.players_player_id_seq'::regclass);
+
+
+--
+-- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.games VALUES (1, 6);
+INSERT INTO public.games VALUES (1, 11);
+
+
+--
+-- Data for Name: players; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.players VALUES (1, 'Kevin');
+
+
+--
+-- Name: players_player_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.players_player_id_seq', 1, true);
+
+
+--
+-- Name: players players_player_id_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.players
+    ADD CONSTRAINT players_player_id_key UNIQUE (player_id);
+
+
+--
+-- Name: games games_player_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_player_id_fkey FOREIGN KEY (player_id) REFERENCES public.players(player_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
